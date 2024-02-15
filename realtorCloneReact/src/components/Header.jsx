dab33231979b180ef55b3a32c6a5b2da55bf7fc7
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-02-15 22:00:56 +0530

    header

--- realtorCloneReact/src/components/Header.jsx
+++ realtorCloneReact/src/components/Header.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-import {useLocation} from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import {useLocation, useNavigate } from 'react-router-dom';
 
 export default function Header() {
+    const [pageState, setPageState] = useState("Sign in");
     const location = useLocation();
-    console.log(location.pathname);
+    const navigate = useNavigate();
+    // const auth = getAuth();
+    
+    // useEffect(() => {
+    //     onAuthStateChanged(auth, (user) => {
+    //         if(user) {
+    //             setPageState("Profile");
+    //         } else {
+    //             setPageState("Sign in");
+    //         }
+    //     });
+    // }, [auth]);
+
+    function pathMatchRoute(route){
+        if(route === location.pathname){
+            return true;
+        }
+    }
+
   return (
     <div className='bg-white border-b shadow-sm sticky top-0 z-40'>
         <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
@@ -12,9 +32,28 @@ export default function Header() {
             </div>
             <div>
                 <ul className='flex space-x-10'>
-                    <li>Home</li>
-                    <li>Offers</li>
-                    <li>Sign in</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                        pathMatchRoute("/") && "text-black border-b-red-500"
+                    }`}
+                    onClick={() => navigate("/")}
+                    >
+                        Home
+                    </li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                        pathMatchRoute("/") && "text-black border-b-red-500"
+                    }`}
+                    onClick={() => navigate("/offers")}
+                    >
+                        Offers
+                    </li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                        (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) && 
+                        "text-black border-b-red-500"
+                    }`}
+                    onClick={() => navigate("/profile")}
+                    >
+                        {/* {pageState} */}
+                    </li>
                 </ul>
             </div>
         </header>
