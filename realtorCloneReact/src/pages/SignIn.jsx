456b59e1b314868ac81a50f22605c7e3ce91479e
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-02-17 23:06:47 +0530

    pass eye

--- realtorCloneReact/src/pages/SignIn.jsx
+++ realtorCloneReact/src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
+import { AiFillEyeInvisible, AiFillEye} from "react-icons/ai"
 import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
@@ -50,7 +51,16 @@ export default function SignIn() {
               placeholder='Password'
               className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'
               />
-              show Password
+              {showPassword ? (
+                <AiFillEyeInvisible
+                  className="absolute right-3 top-3 text-xl cursor-pointer"
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                />
+              ) : (
+                <AiFillEye
+                  className="absolute right-3 top-3 text-xl cursor-pointer"
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                />)}
             </div>
             <div>
               <p>
