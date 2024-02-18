fcf3f54b0b18ca9d7a8fee7619c5b48dfd7918a5
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-02-18 12:13:03 +0530

    OAuth and styling

--- realtorCloneReact/src/pages/SignIn.jsx
+++ realtorCloneReact/src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react';
 import { AiFillEyeInvisible, AiFillEye} from "react-icons/ai"
 import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
@@ -62,19 +63,30 @@ export default function SignIn() {
                   onClick={() => setShowPassword((prevState) => !prevState)}
                 />)}
             </div>
-            <div>
-              <p>
+            <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
+              <p className='mb-6'>
                 Don't have a account?
-                {/* <Link>
+                <Link to="/sign-up"
+                  className='text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1'>
                   Register
-                </Link> */}
+                </Link>
               </p>
-              {/* <p><Link>Forget Password?</Link></p> */}
+              <p>
+                <Link to="/forgot-password" 
+                  className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out'>
+                  Forget Password?
+                </Link>
+              </p>
             </div>
-            <button>Sign in</button>
-            <div>
-              <p>OR</p>
+            <button className='w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800' 
+            type='submit'
+            >
+              Sign in
+            </button>
+            <div className='flex items-center my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300'>
+              <p className='text-centre font-semibold mx-4'>OR</p>
             </div>
+            <OAuth />
           </form>  
         </div>
       </div>
