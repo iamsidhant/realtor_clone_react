72f812fe0b6a50fb1d8886f5cfa87d406fa3ea2f
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-02-20 11:50:30 +0530

    OAuth functionality completed

--- realtorCloneReact/src/components/OAuth.jsx
+++ realtorCloneReact/src/components/OAuth.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { FcGoogle } from "react-icons/fc";
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { toast } from "react-toastify";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function OAuth() {
-    const navigate = useNavigate();
-    async function onGoogleClick(){}
+    const navigate = useNavigate();  
+
+    async function onGoogleClick(){
+      try {
+        const auth = getAuth();
+        const provider = new GoogleAuthProvider();
+        const result = await signInWithPopup(auth, provider);
+        const user = result.user;
+
+        // check for the user
+
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+
+        if (!docSnap.exists()) {
+          await setDoc(docRef, {
+            name: user.displayName,
+            email: user.email,
+            timestamp: serverTimestamp(),
+          });
+        }
+
+        navigate("/");
+      } catch (error) {
+        toast.error("Could not authorize with Google");
+      }
+    }
     
   return (
     <button 
