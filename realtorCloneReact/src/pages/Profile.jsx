44c2ce44c286d324505bb2301a1c9e8943583ac6
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-02-22 17:08:35 +0530

    profile page

--- realtorCloneReact/src/pages/Profile.jsx
+++ realtorCloneReact/src/pages/Profile.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function Profile() {
+  const [formData, setFormData] = useState({
+    name: "sidhant",
+    email: "sidhant@gmail.com",
+  });
+  const { name, email } = formData;
+
   return (
-    <div>Profile</div>
+    <>
+    <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
+      <h1 className='text-3xl text-center mt-6 font-bold'>My Profile</h1>
+      <div className='w-full md:w-[50%] mt-6 px-3'>
+        <form>
+
+          <input type="text" id="name" value={name} disabled className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out" />
+
+          <input type="email" id="email" value={email} disabled className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out" />
+          <div className='flex justify-between'>
+            <p className='flex items-center'>
+              Do you want to change your name?
+              <span></span>
+            </p>
+            <p className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer'>
+              Sign out
+            </p>
+          </div>
+        </form>
+        <button type='submit'
+          className='w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out  hover:shadow-lg active:bg-blue-800'>
+          Sell or rent your home
+        </button>
+      </div>
+    </section>
+    </>
   )
 }
