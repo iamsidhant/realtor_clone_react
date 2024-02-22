a77b8289dc84c42b19d147187fe2a017f387ebd0
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-02-22 23:29:38 +0530

    created private route, custom hook and log out functionality

--- realtorCloneReact/src/pages/Profile.jsx
+++ realtorCloneReact/src/pages/Profile.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { useState } from 'react';
+import {getAuth} from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 export default function Profile() {
+  const auth = getAuth();
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    name: "sidhant",
-    email: "sidhant@gmail.com",
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
   });
   const { name, email } = formData;
+
+  function onLogout(){
+    auth.signOut();
+    navigate("/");
+  }
+
+  function onChange(e){
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id] : e.target.value,
+    }));
+  }
 
   return (
     <>
@@ -15,15 +31,27 @@ export default function Profile() {
       <div className='w-full md:w-[50%] mt-6 px-3'>
         <form>
 
-          <input type="text" id="name" value={name} disabled className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out" />
+          <input 
+            type="text" 
+            id="name" 
+            value={name} 
+            disabled 
+            className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out" />
 
-          <input type="email" id="email" value={email} disabled className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out" />
-          <div className='flex justify-between'>
+          <input 
+            type="email" 
+            id="email" 
+            value={email} 
+            disabled 
+            className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out" />
+          <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6'>
             <p className='flex items-center'>
               Do you want to change your name?
-              <span></span>
+              <span 
+                className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>Edit</span>
             </p>
-            <p className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer'>
+            <p onClick={onLogout}
+              className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer'>
               Sign out
             </p>
           </div>
