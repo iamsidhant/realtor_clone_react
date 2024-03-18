2ae0b4f569cb52c4182fbfe705033b0774ef4690
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-03-18 21:17:18 +0530

    created the spinner component

--- realtorCloneReact/src/pages/Profile.jsx
+++ realtorCloneReact/src/pages/Profile.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import {toast} from "react-toastify"
 import {getAuth, updateProfile} from "firebase/auth";
 import {db} from "../firebase"
-
+import { FcHome } from "react-icons/fc";
 
 export default function Profile() {
   const auth = getAuth();
@@ -90,7 +90,11 @@ export default function Profile() {
         </form>
         <button type='submit'
           className='w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out  hover:shadow-lg active:bg-blue-800'>
-          Sell or rent your home
+            <Link to="/create-listing" 
+            className='flex justify-center items-center'>
+              <FcHome className='mr-2 text-3xl bg-red-200 rounded-full p-1 border-2' />
+              Sell or rent your home
+            </Link>
         </button>
       </div>
     </section>
