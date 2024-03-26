706fbc54d25759074f5e1a559f20bda77b54a289
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-03-26 16:51:38 +0530

    create Listing page

---
+++ realtorCloneReact/src/pages/Listing.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from "react-router-dom";
+import Spinner from '../components/Spinner';
+import { db } from '../firebase';
+import { doc, getDoc } from 'firebase/firestore';
+
+function Listing() {
+    const params = useParams();
+    const [listing, setListing] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchListing(){
+            const docRef = doc(db, "listings", params.listingId);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                setListing(docSnap.data());
+                setLoading(false);
+                console.log(listing);
+            }
+        }
+        fetchListing();
+    }, [params.listingId]);
+    if (loading) {
+        return <Spinner />;
+    }
+    return (
+        <div>{listing.name}</div>
+    )
+}
+
+export default Listing
