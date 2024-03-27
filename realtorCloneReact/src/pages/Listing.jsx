c069634a493b2e51bf996fe07d9bb1267e86e085
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-03-27 12:30:32 +0530

    share copy functionality

--- realtorCloneReact/src/pages/Listing.jsx
+++ realtorCloneReact/src/pages/Listing.jsx
@@ -4,18 +4,21 @@ import Spinner from '../components/Spinner';
 import { db } from '../firebase';
 import { doc, getDoc } from 'firebase/firestore';
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, {
+import SwiperCore from "swiper";
+import {
     EffectFade,
     Autoplay,
     Navigation,
     Pagination
-} from "swiper";
-import "swiper/css";
+} from "swiper/modules";
+import "swiper/css/bundle";
+import { FaShare } from "react-icons/fa";
 
 function Listing() {
     const params = useParams();
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
     SwiperCore.use([Autoplay, Navigation, Pagination]);
 
@@ -37,10 +40,10 @@ function Listing() {
         <main>
             <Swiper
             slidesPerView={1}
-            navigation
+            navigation={true}
             pagination={{ type: "progressbar" }}
             effect='fade'
-            modules={[EffectFade]}
+            modules={[EffectFade, Autoplay, Pagination, Navigation]}
             autoplay={{ delay: 3000 }}
             >
                 {listing.imgUrls.map((url, index) => {
@@ -56,6 +59,23 @@ function Listing() {
                     </SwiperSlide>
                 })}
             </Swiper>
+            <div
+                className='fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center'
+                onClick={() => {
+                    navigator.clipboard.writeText(window.location.href);
+                    shareLinkCopied(true);
+                    setTimeout(() => {
+                        setShareLinkCopied(false);
+                    }, 2000)
+                }}
+                >
+                    <FaShare className='text-lg text-slate-500' />
+            </div>
+            {shareLinkCopied && (
+                <p className='fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2'>
+                    Link Copied
+                </p>
+            )}
         </main>
     )
 }
